97787b1166eab8a177319f256144c6c3603de1d2
Devin Buhl <devin@buhl.casa>
2026-06-01 19:52:54 -0400

    chore: instead of full use docker-cmd-file for mise locks
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-curl -fsSL https://deb.nodesource.com/setup_24.x | sudo bash -
+curl -fsSL https://deb.nodesource.com/setup_24.x | bash -
 apt install -y nodejs
 
 runuser -u ubuntu renovate
